a4fef2419e7d52780b31c969cbbc334ca3196323
Vishal Bhingarde <bhingardevishal5@gmail.com>
2026-05-06 10:40:14 +0530

    Change values of 'a' and 'b' in TernaryOp
    Updated values of variables 'a' and 'b' for testing.

--- Java/Operators/TernaryOp.java
+++ Java/Operators/TernaryOp.java
@@ -2,7 +2,7 @@ package Operators;
 
 public class TernaryOp {
     public static void main(String[] args) {
-        int a = 10, b = 20;
+        int a = 100, b = 200;
         int max = (a > b) ? a : b;   // Returns the larger value
         System.out.println(max); // Output: 20
     }
